cd9bc63e668c8f8749fdf5ce2745f419e30dacca
d h <daiho_hayashi@nifty.com>
2017-11-29 22:13:45 +0900

    module01

--- ygserver/app/kanriDB.js
+++ ygserver/app/kanriDB.js
@@ -1,23 +1,20 @@
+console.log( "*** test.js reloaded." );
 var sqlite3 = require('sqlite3');
 
 var db = new sqlite3.Database("../../kanri.db");
 
-console.log( db.get('select * from shoyu limit 1;'));
 
 //db.each("SELECT * from shoyu limit 3", function(err, row) {
 //      console.log(row);
 //});
 
 
-(function () {
-  console.log( "*** test.js reloaded." );
 
-}());
-exports.wkstr = "hea=r";
-exports.mmm = "aa000000000000000000a";
+//exports.wkstr = "hea=r";
+//exports.mmm = "aa000000000000000000a";
 
-//module.exports.aaa = function () {
-//  return "method aaa";
-//};
+module.exports = {
+  aaa: function (s) { console.log(s); }
+};
 
-module.exports.aaa = "metho aaa called"
+//module.exports.aaa = "metho aaa called"
