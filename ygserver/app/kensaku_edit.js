319c1818e80d219f25fa8a4fb51f173fde2ff221
d h <daiho_hayashi@nifty.com>
2017-12-06 03:55:43 +0900

    kensaku table

--- ygserver/app/kensaku_edit.js
+++ ygserver/app/kensaku_edit.js
@@ -4,6 +4,22 @@ module.exports = {
     toTable: function(owners){
         var row_max = 15;
         console.log(owners.length)
+        var num = owners.length; 
+        var rem;
+        var rows;
+        if ( num > 45){
+            rem = num % 3;
+            rows = (num - rem) / 3 + 1;
+            console.log(owners[1]);
+        }else if(num < 15){
+            rows = num;
+        }else{
+            rows = 15;
+        }
+
+        for(let i = 0; i < rows; i++){
+        //    let col = 
+        }
 
     }
 }
