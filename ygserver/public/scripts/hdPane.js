00b2cd6bf006dcedcc9a7ee7353510be94e166d5
d h <daiho_hayashi@nifty.com>
2018-02-14 01:35:08 +0900

    shomei create btn

--- ygserver/public/scripts/hdPane.js
+++ ygserver/public/scripts/hdPane.js
@@ -1,25 +1,49 @@
-//export function () {
-//$(function(){
-    //$("#aaa").on('click',function(){
-    //    console.log("aaaaaaaaaaaaaaaaaaaa")
-    //});
+var hdPane_show = function(){
+    $("#hdTop").hide();
+    $("#hdSho").css('display', 'inline-grid');;
+};
 
-    //var hdPane_sho = function(str){
-    //    $('header').innerHTML = str;
-    //};
-    var addHdItem = function(){
-        console.log("addHdItem called")
-        $("#hdBtnClear").on('click',function(){
-            //$("#hdItems")[0].textContent = "";
-            var aa = $("#hdItems");
-            console.log(aa);
-            let bt = document.createElement('button');
-            bt.innerText = "bbbbbbb";
-            $(bt).addClass("hdSho hdItem");
-            aa[0].appendChild(bt);
-            //aa[0].
-            //console.log(aa[0].children["1"]);
-        });
-    }
-//});
-//}
+var hdPane_hide = function(){
+    $("#hdBtnClear").on('click',function(){
+        console.log("hd items cleared");
+        $("#hdSho").hide();
+        $("#hdItems").empty();
+        $("#hdTop").show();
+    });
+}
+$(hdPane_hide);
+
+$("#hdBtnCreate").on('click',function($this){
+    var str = $("#hdItems")[0].innerText;
+    console.log(str);
+});
+
+
+
+var addHdItem = function(str){
+    console.log("addHdItem called")
+        //$("#hdItems")[0].textContent = "";
+        var aa = $("#hdItems");
+        console.log(aa);
+        let bt = document.createElement('button');
+        bt.innerText = str;
+        $(bt).addClass("hdSho hdItem");
+        aa[0].appendChild(bt);
+        hdPane_show();
+        //aa[0].
+        //console.log(aa[0].children["1"]);
+}
+
+
+var getKumi = function(){ $('#content').on('click','.tbl',
+    function($this){
+        var s_text = $this.target.innerText;
+        var sho_code = s_text.split(" : ")[1];
+        addHdItem(s_text);
+        //hdPane_sho(s_text);
+        $('#content').load('./sho_kumi?owner=' + sho_code);
+        $(".mainfooter").load("./aaabbb?shoCd="+ sho_code);
+        //console.log("kensaku_name_clicked");
+    });
+}
+
--- ygserver/public/scripts/init.js
+++ ygserver/public/scripts/init.js
@@ -26,42 +26,6 @@ var kenriPanel = function() {
 
 
     getKumi();
-    // = function(){
-        //$('#content').on('click','.tbl', function($this){
-        //    var s_text = $this.target.innerText;
-        //    var sho_code = s_text.split(" : ")[1];
-        //    hdPane_sho(s_text);
-        //    $('#content').load('./sho_kumi?owner=' + sho_code);
-        //    //console.log("kensaku_name_clicked");
-        //});
-    //}
-}
-var hdPane_sho = function(str){
-    console.log(str);
-    $("#hdTop").hide();
-    $("#hdSho").css('display', 'inline-grid');;
-    $("#hdItems")[0].textContent += ("/" + str);
-    //console.log($('#hdItems'));
-};
-
-    /*
-    var kana_sub = function(s_text){
-        $('#content').load('./sho_kumi?owner=' + s_text);
-        
-        console.log(s_text);
-    }
-    */
-//}
-var getKumi = function(){ $('#content').on('click','.tbl',
-    function($this){
-        var s_text = $this.target.innerText;
-        var sho_code = s_text.split(" : ")[1];
-        addHdItem();
-        hdPane_sho(s_text);
-        $('#content').load('./sho_kumi?owner=' + sho_code);
-        $(".mainfooter").load("./aaabbb?shoCd="+ sho_code);
-        //console.log("kensaku_name_clicked");
-    });
 }
 
 $(kenriPanel);
