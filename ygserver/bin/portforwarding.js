4e5a75d10ea9dd79c57cd21338a4ba01f907fd7b
imdebop <daiho_hayashi@nifty.ocm>
2018-02-26 16:29:13 +0900

    proxy rules

--- ygserver/bin/portforwarding.js
+++ ygserver/bin/portforwarding.js
@@ -1,14 +1,17 @@
 var httpProxy = require('http-proxy');
+var HttpProxyRules = require('http-proxy-rules');
+
 var options = {
     //hostnameOnly:true,
-    target: 'http://localhost:3000',
+    //target: 'http://localhost:3000',
     router: {
+        '192.168.253.159:8080': 'http://localhost:3000',
         //forward: 'localhost:8080'
         //'hoge.example.com': '127.0.0.1:9090'
     }
 
 }
-httpProxy.createServer(options).listen(8080)
+httpProxy.createServer(options).listen(8080);
 
 
 //var forward = require('http-port-forward');
