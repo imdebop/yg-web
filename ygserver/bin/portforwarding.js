239b9c28894b3bf2ae1cc2cf9803809ac79ff184
imdebop <daiho_hayashi@nifty.ocm>
2018-08-10 13:24:35 +0900

    port

--- ygserver/bin/portforwarding.js
+++ ygserver/bin/portforwarding.js
@@ -5,7 +5,7 @@ var options = {
     //hostnameOnly:true,
     //target: 'http://localhost:3000',
     router: {
-        '192.168.253.159:8080': 'http://localhost:3000',
+        '192.168.253.158:8080': 'http://localhost:3000',
         //forward: 'localhost:8080'
         //'hoge.example.com': '127.0.0.1:9090'
     }
